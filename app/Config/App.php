31f78684465f33040ceb90b0bcd400a58c7af7cc
Rei Batoon <rei.batoon24@gmail.com>
2025-03-25 18:01:39 +0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/TX32-24-GRP5-BLUEY/';
+    public string $baseURL = 'http://localhost/FiorellaWebsite/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
